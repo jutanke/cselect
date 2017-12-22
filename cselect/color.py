1eeef2c644bdb20cdfaeefc091b73a21a13cb372
Julian <jutanke@googlemail.com>
2017-12-22 16:15:05 +0000

    make poisson sampling for LAB

--- cselect/color.py
+++ cselect/color.py
@@ -1,7 +1,8 @@
 import numpy as np
 from skimage import color
 import numpy as np
-from random import uniform
+from scipy.spatial import KDTree
+from random import random, uniform, randint
 
 def lincolor(n, random_sat=False, random_val=False):
     """ returns linearly sampled colors from HSV space
@@ -15,3 +16,40 @@ def lincolor(n, random_sat=False, random_val=False):
     HSV = np.array([HSV], 'float64')
     RGB = color.hsv2rgb(HSV) * 255
     return np.squeeze(RGB).astype('uint8')
+
+def poisson_disc_sampling(n, gen_candidate=None, n_candidates=150):
+    """ generate random-evenly spaced points
+    """
+    if gen_candidate is None:
+        gen_candidate = lambda: (random(), random())  # between 0..1
+    
+    data = [gen_candidate()]
+    
+    for _ in range(1, n):
+        lookup = KDTree(data)
+        
+        best_cand = None
+        best_dist = 0
+        
+        for _ in range(n_candidates):  # find best candidate
+            cand = gen_candidate()
+            dist, _ = lookup.query(cand)
+            if best_dist < dist:
+                best_dist = dist
+                best_cand = cand
+        assert best_cand is not None  # make sure best candidate exists
+        data.append(best_cand)
+    
+    return data
+
+def poisson_disc_sampling_Lab(n):
+    """ randomly sample colors from Lab space
+    """
+    gen_ab_candidate = lambda: \
+        (uniform(30, 100), uniform(-127, 128), uniform(-127, 128))
+    Samples = poisson_disc_sampling(n, gen_candidate=gen_ab_candidate)
+    Lab = np.array([(L, a, b) for L,a,b in Samples])
+    Lab = np.array([Lab], 'float64')  # elevate dims to fit conversion
+                                      # libraries API
+    RGB = color.lab2rgb(Lab) * 255
+    return np.squeeze(RGB.astype('uint8'))
